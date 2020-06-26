93253f3b6d01fb66ed3d94a868e0a68158e8a32d
Josue Narro <josue.narro.a@gmail.com>
2020-06-26 02:55:57 -0500

    Create board function

--- js/myBoards.js
+++ js/myBoards.js
@@ -1,1 +1,37 @@
-//local storage
+// local storage
+
+// Board section
+const modal = document.getElementById('myModal');
+const modalBtn = document.querySelector('.boards__modal-button');
+const span = document.getElementsByClassName('close')[0];
+
+modalBtn.onclick = function() {
+  modal.style.display = 'block';
+};
+
+span.onclick = function() {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
+
+const boardBtn = document.getElementById('board__create-button');
+boardBtn.addEventListener('click', createBoard);
+
+function createBoard() {
+  const boardTitle = document.getElementById('board-title');
+  const board = document.createElement('div');
+  board.classList.add('board');
+  board.innerHTML = `
+    <h1 className="board-title">${boardTitle.value}</h1>
+    <div className="board__star-container">
+      <img className="board__star" src="assets/star.svg" alt="" />
+    </div>
+  `;
+  document.querySelector('.boards-container').prepend(board);
+}
